427306915c717335c665997b1627ddd81fbdb0ad
Ngosch <carl1312@gmail.com>
2023-09-30 10:42:49 +0900

    Web Api 3

--- app.py
+++ app.py
@@ -5,5 +5,24 @@ zipcode = "0287111"
 
 response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}")
 
-print(response)
-print(response.text)
+# print(response)
+# print(response.text)
+
+# 辞書として表示
+dic = response.json()
+# print(dic)
+# print(f"{dic['results'][0]['address1']}{dic['results'][0]['address2']}{dic['results'][0]['address3']}")
+address_info = response.json()["results"][0]
+
+prefecture_name = address_info["address1"]
+city_name = address_info["address2"]
+town_name = address_info["address3"]
+
+address = f"{prefecture_name}{city_name}{town_name}"
+print(address)
+
+
+
+# # typeを使ってjsonを調べる
+# print(type(response.json()))
+
