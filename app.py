07a346b96f45ae3c96da2f8c81844b6c7cc7d5b9
Ngosch <carl1312@gmail.com>
2023-09-30 09:42:06 +0900

    インプットテスト後

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import requests
 
-response = requests.get("https://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111")
+# zipcode = input("郵便番号<ハイフン無し7桁>は？")
+zipcode = "0287111"
+
+response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}")
 
 print(response)
 print(response.text)
